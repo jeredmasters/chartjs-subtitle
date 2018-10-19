08f6c10639a4f9ebfa01b713c1938c180284c669
Jered Masters <jered.masters@outlook.com>
2018-10-19 17:20:02 +0800

    added logic for different positions and multi-line

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Chart.js Subtitle Plugin
 [NPM Package][npm-url]
+
 Simple subtitle plugin for [Chart.Js][chartjs]
+
+Works with all four title positions and multi-line titles.
+See more information on standard [title options](https://www.chartjs.org/docs/latest/configuration/title.html)
 
 ## Install
 ```bash
@@ -53,6 +57,13 @@ The options for the subtitle are based on the options for the existing title
         fontStyle: 'normal',
 
         /**
+         * Padding between the title and the subtitle
+         * @member {Number}
+         * @default 4
+         */
+        paddingTop: 4,
+
+        /**
          * Subtitle text to display
          * @member {String}
          * @default ''
@@ -69,7 +80,8 @@ The options for the subtitle are based on the options for the existing title
 ![image](https://user-images.githubusercontent.com/10936951/47205090-8e9e1200-d3b7-11e8-91cc-b81a572c8283.png)
 ![image](https://user-images.githubusercontent.com/10936951/47205101-9a89d400-d3b7-11e8-8098-bdb8acf3a5b4.png)
 ![image](https://user-images.githubusercontent.com/10936951/47205132-a9708680-d3b7-11e8-975f-58f35e88d67b.png)
-
+![image](https://user-images.githubusercontent.com/10936951/47209222-62d45980-d3c2-11e8-8de3-d16d9aa1f3d2.png)
+![image](https://user-images.githubusercontent.com/10936951/47209273-81d2eb80-d3c2-11e8-998f-53c7c5fa8561.png)
 
 ## Building
 
--- src/plugin.js
+++ src/plugin.js
@@ -41,6 +41,13 @@ const defaultOptions = {
   fontStyle: 'normal',
 
   /**
+   * Padding between the title and the subtitle
+   * @member {Number}
+   * @default 4
+   */
+  paddingTop: 4,
+
+  /**
    * Subtitle text to display
    * @member {String}
    * @default ''
@@ -80,24 +87,111 @@ const SubtitlePlugin = {
   },
 
   /**
-   * plugin hook to draw the sub title
+   * Draw the subtitle on the top position
+   * @param {Chart} chart
+   * @param {Object} options
+   */
+  drawTop(chart, options) {
+    const { text } = options;
+    const { ctx, width } = chart;
+
+    // this value accounts for multiple lines in title
+    const titleOffset = chart.titleBlock.height - chart.options.title.padding;
+
+    const textX = Math.round((width - ctx.measureText(text).width) / 2);
+    const textY = titleOffset + options.paddingTop + 3;
+    ctx.fillText(text, textX, textY);
+  },
+
+  /**
+   * Draw the subtitle on the left position
+   * @param {Chart} chart
+   * @param {Object} options
+   */
+  drawLeft(chart, options) {
+    const { text } = options;
+    const { ctx, height } = chart;
+
+    // this value accounts for multiple lines in title
+    const titleOffset = chart.titleBlock.width - chart.options.title.padding;
+
+    ctx.save();
+    ctx.translate(0, 0);
+    ctx.rotate(-Math.PI / 2);
+
+    const textX = Math.round((height + ctx.measureText(text).width) / 2);
+    const textY = titleOffset + options.paddingTop + 3;
+
+    ctx.fillText(text, -textX, textY);
+    ctx.restore();
+  },
+
+  /**
+   * Draw the subtitle on the right position
+   * @param {Chart} chart
+   * @param {Object} options
+   */
+  drawRight(chart, options) {
+    const { text } = options;
+    const { ctx, height, width } = chart;
+
+    // this value accounts for multiple lines in title
+    const titleOffset = chart.titleBlock.width - chart.options.title.padding;
+
+    ctx.save();
+    ctx.translate(0, 0);
+    ctx.rotate(Math.PI / 2);
+
+    const textX = Math.round((height - ctx.measureText(text).width) / 2);
+    const textY = (titleOffset + options.paddingTop) - width;
+
+    ctx.fillText(text, textX, textY);
+    ctx.restore();
+  },
+
+  /**
+   * Draw the subtitle on the bottom position
+   * @param {Chart} chart
+   * @param {Object} options
+   */
+  drawBottom(chart, options) {
+    const { text } = options;
+    const { ctx, height, width } = chart;
+
+    const textX = Math.round((width - ctx.measureText(text).width) / 2);
+    const textY = (height - chart.options.title.padding * 2) + (options.paddingTop + 11);
+    ctx.fillText(text, textX, textY);
+  },
+
+  /**
+   * Plugin hook to draw the sub title
    * @param chart chartjs instance
    * @param easingValue animation function
    * @param options plugin options
    */
   beforeDraw(chart, easingValue, rawOptions) {
+    console.log(chart);
     const options = Object.assign({}, defaultOptions, rawOptions);
     if (options.display) {
-      const { text } = options;
-      const { width, ctx } = chart.chart;
+      const { ctx } = chart.chart;
       ctx.restore();
       ctx.font = this.resolveFont(options);
       ctx.textBaseline = 'middle';
-      const textX = Math.round((width - ctx.measureText(text).width) / 2);
-      const textY = 31;
       ctx.fillStyle = options.fontColor;
-      ctx.fillText(text, textX, textY);
-      ctx.save();
+      switch (chart.options.title.position) {
+        default:
+          this.drawTop(chart.chart, options);
+          break;
+        case 'left':
+          this.drawLeft(chart.chart, options);
+          break;
+        case 'right':
+          this.drawRight(chart.chart, options);
+          break;
+        case 'bottom':
+          this.drawBottom(chart.chart, options);
+          break;
+      }
     }
   },
 };
